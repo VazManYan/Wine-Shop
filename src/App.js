93856882ce04e970f0a245d56bc120beece2ffc9
VazManYan <vazgenar.1993@gmail.com>
2023-03-20 18:24:54 +0200

    addd

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+
 function App() {
   return (
     <div className="App">  
